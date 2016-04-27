bd2708acac2e03ba87bedd4749b16b1375bbb944
XiaoJIaQI <xiaojiaqi.cn@gmail.com>
2016-04-27 10:39:48 +0800

    Update install.sh

--- package/install.sh
+++ package/install.sh
@@ -1,5 +1,7 @@
-yum install -y gcc gcc-c++ autoconf automake libtool vim 
+yum install -y gcc gcc-c++ autoconf automake libtool vim  wget
 
 
 curl -O -L https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz -kvv
+
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 
