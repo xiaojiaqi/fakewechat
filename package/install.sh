754069fe211ffd5b41ec25fe63655eb824e68937
XiaoJIaQI <xiaojiaqi.cn@gmail.com>
2016-04-27 22:18:00 +0800

    Update install.sh

--- package/install.sh
+++ package/install.sh
@@ -41,6 +41,10 @@ cp src/redis-server ~/gopath/src/github.com/fakewechat/bin
 
 
 
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
+
+yum install -y ansible 
 
 
 
